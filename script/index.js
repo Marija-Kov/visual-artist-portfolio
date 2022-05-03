29b7042aef7b29635489cf01baa5d90e8976a348
Marija K <kccmrj@outlook.com>
2022-05-03 23:49:41 +0200

    logo animation update

--- script/index.js
+++ script/index.js
@@ -1,5 +1,13 @@
 let hamMenu = document.querySelector('#menu-toggle');
+let logoParts = document.querySelectorAll('#logo>span');
+let logo = document.querySelector("#logo");
 
 hamMenu.addEventListener('click', ()=> {
     hamMenu.classList.contains('open') ? hamMenu.classList.remove('open') : hamMenu.classList.add('open');
 })
+
+window.addEventListener('scroll', ()=> {
+    logo.classList.add('apart');
+    setTimeout(() => {logo.classList.remove('apart')}, 3000)
+})
+
