9595e6b50082f97d2ce77643e4fe932eafbf9e09
Marija K <kccmrj@outlook.com>
2022-05-08 09:53:04 +0200

    nicer load animation

--- script/index.js
+++ script/index.js
@@ -1,9 +1,13 @@
 let hamMenu = document.querySelector('#menu-toggle');
 let logoParts = document.querySelectorAll('#logo>span');
 let logo = document.querySelector("#logo");
+let nav = document.querySelector('nav');
 
 hamMenu.addEventListener('click', ()=> {
     hamMenu.classList.contains('open') ? hamMenu.classList.remove('open') : hamMenu.classList.add('open');
+    hamMenu.classList.contains("open")
+      ? nav.setAttribute("style", "display: inline-block")
+      : nav.setAttribute("style", "display: none");
 })
 
 window.addEventListener('scroll', ()=> {
