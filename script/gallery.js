57c64e9792614da0a3d18c1580c9cbf8ba5a912e
Marija K <kccmrj@outlook.com>
2022-05-09 17:31:51 +0200

    gallery layout done

---
+++ script/gallery.js
@@ -0,0 +1,6 @@
+let thumbs = document.querySelector('.thumbs');
+for(let i = 0; i< 16 ; ++i){
+let thumb = document.createElement('div');
+thumb.classList.add('thumb');
+thumbs.appendChild(thumb);
+}
