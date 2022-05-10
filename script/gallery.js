f9f7445a07ec5f0d9891dbf451863a91d326e316
Marija K <kccmrj@outlook.com>
2022-05-10 07:50:00 +0200

    data fetched and loaded into gallery

--- script/gallery.js
+++ script/gallery.js
@@ -1,6 +1,24 @@
+let url = "/pages/data.json";
+
 let thumbs = document.querySelector('.thumbs');
-for(let i = 0; i< 16 ; ++i){
-let thumb = document.createElement('div');
-thumb.classList.add('thumb');
-thumbs.appendChild(thumb);
-}
+
+fetch(url)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+      return response.json();
+    }
+  })
+  .then(data => showData(data))
+  .catch(err => `Error: ${err}`);
+  
+
+  async function showData(data){
+     for (let i = 0; i < data._3d.length; ++i) {
+       let thumb = document.createElement("div");
+       thumb.classList.add("thumb");
+       thumb.setAttribute('style', `background-image: url(${data._3d[i].images[0]}); background-size: 160%; background-position-x: center;`);
+       thumbs.appendChild(thumb);
+     } 
+  }
