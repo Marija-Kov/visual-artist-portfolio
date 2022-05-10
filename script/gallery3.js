5599d5ae4142e586377f2bc57241bdbcd25db669
Marija K <kccmrj@outlook.com>
2022-05-10 11:39:17 +0200

    gallery update

---
+++ script/gallery3.js
@@ -0,0 +1,24 @@
+let url = "/pages/data.json";
+
+let thumbs = document.querySelector('.thumbs');
+
+fetch(url)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+      return response.json();
+    }
+  })
+  .then(data => showData(data))
+  .catch(err => `Error: ${err}`);
+  
+
+  async function showData(data){
+     for (let i = 0; i < data.embroidery.length; ++i) {
+       let thumb = document.createElement("div");
+       thumb.classList.add("thumb");
+       thumb.setAttribute('style', `background-image: url(${data.embroidery[i].images[0]}); background-size: 160%; background-position-x: center;`);
+       thumbs.appendChild(thumb);
+     } 
+  }
