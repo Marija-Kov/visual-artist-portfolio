f58d15a4962e6345c9fc35cd279f84402da6d0ec
Marija K <kccmrj@outlook.com>
2022-11-28 10:55:14 +0100

    index refactored

---
+++ script/index.js
@@ -0,0 +1,9 @@
+
+    setTimeout(() => {
+      document.querySelectorAll(".line").forEach((e) => {
+        e.style.opacity = "1";
+      });
+    }, 2000);
+    hamburgerMenuDisplay();
+
+import { hamburgerMenuDisplay } from "./ham-navigation.js";
