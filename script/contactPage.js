0f37110b6e6edbca638485f9fba37f5cffad614b
Marija K <kccmrj@outlook.com>
2023-03-12 22:40:25 +0100

    removing useless code

--- script/contactPage.js
+++ script/contactPage.js
@@ -1,9 +1,6 @@
 
 hamburgerMenuDisplay();
 sendMessage();
-setTimeout(() => {
- document.querySelector(".social").style.opacity = "1";
-}, 1200); 
 
 import { hamburgerMenuDisplay } from "./ham-navigation.js";
 import { sendMessage } from "./sendMessage.js";
